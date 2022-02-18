4f8b407df65c2ae7c1393cbc94b3edb216864462
Silkaye <91874661+Silkaye@users.noreply.github.com>
2022-02-18 12:04:49 +1000

    Added Splash screen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-const hamburger = document.querySelector('.hamburger')
-const mobile_menu = document.querySelector('.mobile-nav')
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 2000);
+})
 
 hamburger.addEventListener('click', function () 
 {
